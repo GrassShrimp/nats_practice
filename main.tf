b25b13846d016fa4abb1f11e52cf8d69537d5706
劉品均 <grassshrimp@liupinjunde-MacBook-Pro.local>
2021-01-31 00:17:38 +0800

    install nats-streaming

--- main.tf
+++ main.tf
@@ -19,3 +19,30 @@ resource "helm_release" "nats-operator" {
 
   create_namespace = true
 }
+
+resource "helm_release" "nats-streaming" {
+  depends_on = [ helm_release.nats-operator ]
+
+  name       = "nats-stan"
+  repository = "https://nats-io.github.io/k8s/helm/charts/"
+  chart      = "stan"
+  version    = "0.7.4"
+  namespace  = "nats-io"
+
+  values = [
+    <<EOF
+    stan:
+      clusterID: demo
+      nats:
+        url: nats://nats-cluster.nats-io.svc.cluster.local:4222
+      logging:
+        debug: true
+        trace: true
+    store:
+      volume:
+        storageClass: hostpath
+    EOF
+  ]
+  
+  create_namespace = true
+}
